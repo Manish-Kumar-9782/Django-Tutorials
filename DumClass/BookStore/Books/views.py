f85111f216caeefa47f501cec27693023e2f20da
Manish Kumar <manishkumar9782@gmail.com>
2023-05-20 22:12:47 +0530

    worked on the static file and link, added students to database

--- DumClass/BookStore/Books/views.py
+++ DumClass/BookStore/Books/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 # Create your views here.
 
+
 def home(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 
 def add_book(request):
-    
+
     if request.method == "GET":
         print("sending add book page to the client....")
         return render(request, 'addBook.html')
-        
+
     if request.method == "POST":
         print("receiving add book request from the client....")
-        
-        print("Receiving Data: ", request.POST)
+        print("\n\n==============================================")
+        # print("Receiving Data: ", request.POST)
+        # book-title, book-author, book-price, book-pages
+        bookTitle = request.POST['book-title']
+        bookAuthor = request.POST['book-author']
+        bookPages = request.POST['book-pages']
+        bookPrice = request.POST['book-price']
+
+        data = {'title': bookTitle, 'author': bookAuthor,
+                'pages': bookPages, 'price': bookPrice}
+
+        print("received book Data: ", data)
+
+        book = Book()
+        book.title = bookTitle
+        book.author = bookAuthor
+        book.pages = bookPages
+        book.price = bookPrice
+        book.save()
+
+        print("==============================================\n\n")
+
         return render(request, 'index.html')
-    
