64f55a561e73b0b2d47d2cd0cee709ca3dabe158
Manish Kumar <manishkumar9782@gmail.com>
2024-05-15 11:52:59 +0530

    worked on the CURD Operation, completed Create and Read Operation.

--- DemoApp/Employee/urls.py
+++ DemoApp/Employee/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import emp_view, add_employee
+from .views import emp_view, add_employee, emp_update
 
 urlpatterns = [
     path("", view=emp_view, name="emp_view"),
     path("add/", view=add_employee, name="add_emp"),
+    path("modify/<int:emp_id>", view=emp_update, name="modify_emp"),
 
 ]
--- DemoApp/Employee/views.py
+++ DemoApp/Employee/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
 
 # Create your views here.
 
 
 def emp_view(request):
-    return HttpResponse("Hello, employee View")
+
+    employees = Employee.objects.all()
+
+    return render(request, "view_employee.html", {"Employees": employees})
 
 
 def add_employee(request):
@@ -14,6 +18,19 @@ def add_employee(request):
         return render(request, "add_employee.html")
 
     if request.method == "POST":
-        print(request.POST)
+
+        _first_name = request.POST["first_name"]
+        _last_name = request.POST["last_name"]
+        _phone_number = request.POST["phone_number"]
+        _email = request.POST["email"]
+
+        emp = Employee(first_name=_first_name, last_name=_last_name,
+                       phone_number=_phone_number, email=_email)
+        emp.save()
 
     return HttpResponse("Hello, employee View")
+
+
+def emp_update(request, emp_id):
+
+    return HttpResponse(f"Modify employee by id: %s" % emp_id)
