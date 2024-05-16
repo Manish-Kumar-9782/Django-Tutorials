b23bdd4e2051a264730a760bcd78fd60289b34e6
Manish Kumar <manishkumar9782@gmail.com>
2024-05-16 11:52:43 +0530

    Worked on CURD, completed update and delete operation

--- DemoApp/Employee/urls.py
+++ DemoApp/Employee/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import emp_view, add_employee, emp_update
+from .views import emp_view, add_employee, emp_update, emp_remove
 
 urlpatterns = [
     path("", view=emp_view, name="emp_view"),
     path("add/", view=add_employee, name="add_emp"),
     path("modify/<int:emp_id>", view=emp_update, name="modify_emp"),
+    path("remove/<int:emp_id>", view=emp_remove, name="remove_emp"),
 
 ]
--- DemoApp/Employee/views.py
+++ DemoApp/Employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Employee
 
@@ -15,7 +15,7 @@ def emp_view(request):
 def add_employee(request):
 
     if request.method == "GET":
-        return render(request, "add_employee.html")
+        return render(request, "employee_form.html")
 
     if request.method == "POST":
 
@@ -28,9 +28,37 @@ def add_employee(request):
                        phone_number=_phone_number, email=_email)
         emp.save()
 
-    return HttpResponse("Hello, employee View")
+    return redirect("emp_view")
 
 
 def emp_update(request, emp_id):
 
-    return HttpResponse(f"Modify employee by id: %s" % emp_id)
+    if request.method == "GET":
+        emp = Employee.objects.get(id=emp_id)
+        return render(request, "employee_form.html", {"update": True, "Emp": emp})
+
+    if request.method == "POST":
+        _first_name = request.POST["first_name"]
+        _last_name = request.POST["last_name"]
+        _phone_number = request.POST["phone_number"]
+        _email = request.POST["email"]
+
+        # Now i need to access the employee with id.
+        emp = Employee.objects.get(id=emp_id)
+
+        emp.first_name = _first_name
+        emp.last_name = _last_name
+        emp.phone_number = _phone_number
+        emp.email = _email
+
+        emp.save()
+
+        return redirect("emp_view")
+
+
+def emp_remove(request, emp_id):
+
+    if request.method == "GET":
+        Employee.objects.get(id=emp_id).delete()
+
+    return redirect("emp_view")
