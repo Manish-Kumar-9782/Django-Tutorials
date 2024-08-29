567350ab0306984e16cdf4a46cea7e22edb75dae
Manish Kumar <manishkumar9782@gmail.com>
2024-08-29 12:04:30 +0530

    setup of template, data in template

--- Sep 2024/Demo/Demo/views.py
+++ Sep 2024/Demo/Demo/views.py
@@ -1,37 +1,26 @@
-from django.http import HttpResponse
-from data import myContent
+from django.http import HttpResponse  # not in use
+from django.shortcuts import render
+from .data import myContent
 
 
 def home_page(request):
-    entries = ""  # to store all
-    for item in myContent:
-        row = f"""
-            <tr> 
-                <td>{item["name"]}</td>
-                <td>{item["age"]}</td>
-                <td>{item["height"]}</td>
-                <td>{item["bloodgroup"]}</td>
-            </tr>
-        """
-        entries += row + "\n"
 
-    html_code = f"""
-        <h1>Hello world</h1>
-        <table> 
-            <thead> 
-                <tr>
-                    <th>name</th>
-                    <th>age</th>
-                    <th>height</th>
-                    <th>bloodGroup</th>
-                </tr>
-            </thead>
+    groups = {
+        "20_to_30": 0,
+        "30_to_40": 0,
+        "40_plus": 0
+    }
 
-            <tbody> 
-                {entries}
-            </tbody>
 
-        </table>
-    """
-    print(html_code)
-    return HttpResponse(html_code)
+# create age groups
+    for person in myContent:
+
+        if person["age"] >= 20 and person["age"] < 30:
+            groups["20_to_30"] += 1
+        elif person["age"] >= 30 and person["age"] < 40:
+            groups["30_to_40"] += 1
+        elif person["age"] > 40:
+            groups["40_plus"] += 1
+
+    # now i don't want to use HttpResponse
+    return render(request, "records.html", {"Persons": myContent, "ageGroups": groups})
