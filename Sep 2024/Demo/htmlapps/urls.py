213da3d78be42b31a9973ca5c6c377eb4f77a67b
Manish Kumar <manishkumar9782@gmail.com>
2024-09-16 12:34:05 +0530

    worked on the input form

--- Sep 2024/Demo/htmlapps/urls.py
+++ Sep 2024/Demo/htmlapps/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import table_extra, button_test
+from .views import table_extra, button_test, position_property, card_example_1, user_registration
 
 urlpatterns = [
     path("table_extra", view=table_extra, name='table_extra'),
     path("button_test", view=button_test, name='button_test'),
+    path("position_property", view=position_property, name='position_property'),
+    path("card_example_1", view=card_example_1, name='card_example_1'),
+    path("user_registration", view=user_registration, name='user_registration'),
 ]
--- Sep 2024/Demo/htmlapps/views.py
+++ Sep 2024/Demo/htmlapps/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def user_registration(request):
+
+    print("request data: ", request.GET)
+
+    username = request.GET["username"]
+    phone_number = request.GET["phone_number"]
+    email = request.GET["email"]
+    password = request.GET["password"]
+    c_password = request.GET["c_password"]
+
+    print("Name : ", username)
+    print("phone_number : ", phone_number)
+    print("email : ", email)
+    print("password : ", password)
+    print("c_password : ", c_password)
+
+    return render(request, "html_pages/userRegistration.html")
+
+
 def table_extra(request):
 
     return render(request, "html_pages/table_extra.html")
@@ -10,3 +29,11 @@ def table_extra(request):
 
 def button_test(request):
     return render(request, "html_pages/button.html")
+
+
+def position_property(request):
+    return render(request, "html_pages/position.html")
+
+
+def card_example_1(request):
+    return render(request, "html_pages/card_example1.html")
