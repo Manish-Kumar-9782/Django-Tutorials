062859f6ffd387806a0d8ff4a26f1f48902bb706
Manish Kumar <manishkumar9782@gmail.com>
2024-10-09 11:44:00 +0530

    worked on update and delete a task from a specific TaskList

--- Sep 2024/TaskManager/TaskList/urls.py
+++ Sep 2024/TaskManager/TaskList/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import task_list, add_taskList, add_task, delete_task, update_taskList_tasks
+from .views import task_list, add_taskList, add_task, delete_taskList, update_taskList_tasks, delete_task, update_task
 
 
 urlpatterns = [
     path("", view=task_list, name='view_tasks'),
     path("add", view=add_taskList, name='add_taskList'),
     path("add_task/<int:taskListId>", view=add_task, name='add_task'),
-    path("delete_task/<int:taskListId>", view=delete_task, name='delete_task'),
+    path("delete_taskList/<int:taskListId>",
+         view=delete_taskList, name='delete_taskList'),
     path("update_taskList_tasks", view=update_taskList_tasks,
          name='update_taskList_tasks'),
+
+    path("delete_task/<int:taskId>", view=delete_task, name="delete_task"),
+    path("update_task/<int:taskId>", view=update_task, name="update_task"),
+
 ]
--- Sep 2024/TaskManager/TaskList/views.py
+++ Sep 2024/TaskManager/TaskList/views.py
@@ -31,29 +31,6 @@ def add_taskList(request):
         return redirect("view_tasks")
 
 
-def add_task(request, taskListId):
-    # here weill save task_list's task in Task model
-    # using TaskList object.
-    if request.method == "POST":
-        text = request.POST.get("text")
-        # first retrieve the TaskList
-        taskList = TaskList.objects.get(id=taskListId)
-        task = Task(text=text, task_list=taskList)
-        task.save()
-        return redirect("view_tasks")
-
-    return HttpResponse("<h1>Delete request only supports POST request</h1>")
-
-
-def delete_task(request, taskListId):
-
-    if request.method == "POST":
-        task = TaskList.objects.get(id=taskListId)
-        task.delete()
-
-        return redirect("view_tasks")
-
-
 def update_taskList_tasks(request):
 
     if request.method == "POST":
@@ -70,4 +47,57 @@ def update_taskList_tasks(request):
             task.isCompleted = task_isCompleted  # updating task isCompleted status
             task.save()
 
+        return redirect("view_tasks")
+
+
+def delete_taskList(request, taskListId):
+
+    if request.method == "POST":
+        task = TaskList.objects.get(id=taskListId)
+        task.delete()
+
+        return redirect("view_tasks")
+
+# =====================================================================#
+
+
+def add_task(request, taskListId):
+    # here weill save task_list's task in Task model
+    # using TaskList object.
+    if request.method == "POST":
+        text = request.POST.get("text")
+        # first retrieve the TaskList
+        taskList = TaskList.objects.get(id=taskListId)
+        task = Task(text=text, task_list=taskList)
+        task.save()
+        return redirect("view_tasks")
+
+    return HttpResponse("<h1>Delete request only supports POST request</h1>")
+
+
+def delete_task(request, taskId):
+
+    if request.method == "GET":
+        task = Task.objects.get(id=taskId)
+        task.delete()
+        return redirect("view_tasks")
+
+
+def update_task(request, taskId):
+
+    if request.method == "GET":
+        task = Task.objects.get(id=taskId)
+        # taskListId = task.task_list_id
+        taskLists = TaskList.objects.all()
+
+        return render(request, 'home.html',
+                      {"taskLists": taskLists, "update": {
+                          "task": task, "taskListId": task.task_list.id}},
+                      )
+
+    if request.method == "POST":
+        text = request.POST.get("text")
+        task = Task.objects.get(id=taskId)
+        task.text = text
+        task.save()
         return redirect("view_tasks")
